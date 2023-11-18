a4a92b58b29a59c3c946b5d78471de69722124de
高世波 <2783626707@qq.com>
2023-11-18 14:34:32 +0800

    格式化代码

---
+++ 7BST&AVL/linked_bst_avl.h
@@ -0,0 +1,205 @@
+#include<iostream>
+
+using namespace std;
+
+typedef struct node{
+  int data;
+  node *left;
+  node *right;
+  node *parent;
+}node;
+
+typedef struct nodeArr{
+  node **arr;
+  int maxsize;
+  int pos = 0;
+}nodeArr;
+
+class BST {
+protected:
+  nodeArr *arr;   // 用来记录节点的数组，好像没什么用（但好像又有点用
+  void deleteTree(node *p);
+  int getSL(node *p, int k = 1);
+  void lnrdfs(node *p, nodeArr *arr);
+  node *addChild(int data, node *parent = NULL, bool addleft = true);
+  node *getNodeByData(node *root, int x);
+  bool deleteNodeByPos(node *pos);
+public:
+  node *root;       // 根节点
+  int nodeNums = 0; // 节点数目
+  BST(int data);
+  bool insertNode(int data);            // 插入元素
+  bool deleteNode(int data);            // 删除元素
+  void generateBST(int list[], int n);  // 从数组生成树
+  int *LNRTree();                       // 中序遍历
+  float getASL();                       // 获取平均查找长度
+  ~BST();
+};
+
+class AVL : public BST {
+public:
+  AVL(int data) : BST(data) {};
+  void generateAVL(int list[], int n);
+};
+
+//解析析构
+BST::BST(int data){
+  root = new node; //初始化树一并初始化根节点
+  nodeNums++;
+  root->data = data;
+  root->left = NULL;
+  root->right = NULL;
+  root->parent = NULL;
+}
+
+void BST::deleteTree(node *p){
+  if(p == NULL) return;
+  deleteTree(p->left);
+  deleteTree(p->right);
+  delete p;
+}
+
+BST::~BST(){
+  deleteTree(root);
+}
+
+//插入、删除元素以及生成树相关
+node *BST::addChild(int data, node *parent, bool addleft){
+  node *child = new node;
+  child->data = data;
+  child->left = NULL;
+  child->right = NULL;
+  child->parent = parent;
+  if(addleft){
+    parent->left = child;
+  }else{
+    parent->right = child;
+  }
+  nodeNums++;
+  return child;
+}
+
+bool BST::insertNode(int data){
+  node *p = root;
+  while(true){
+    if(data < p->data){
+      if(p->left != NULL) p = p->left;
+      else{
+        p->left = addChild(data, p, true);
+        break;
+      }
+    }else if(data > p->data){
+      if(p->right != NULL) p = p->right;
+      else{
+        p->right = addChild(data, p, false);
+        break;
+      }
+    }else{
+      return false; //有相同元素返回false
+    }
+  }
+  return true;
+}
+
+void BST::generateBST(int list[], int n){
+  for(int i = 1; i < n; i++){
+    insertNode(list[i]);
+  }
+}
+
+node * BST::getNodeByData(node *p, int x){
+  if(p == NULL) return 0;
+  if(p->data > x) return getNodeByData(p->left, x);
+  else{
+    if(p->data == x) return p;
+    else return getNodeByData(p->right, x);
+  }
+}
+
+bool BST::deleteNodeByPos(node *pos){
+  if(pos == NULL) return false;
+  nodeNums--;
+  node *p = pos->left;
+  node *q = pos->right;
+  node *r = q;
+  if(p == NULL && q == NULL){
+    //不做任何操作
+  }else if(p == NULL){
+    pos->data = q->data;
+    pos->right = q->right;
+    pos->left = q->left;
+    pos = q;
+  }else if(q == NULL){
+    pos->data = p->data;
+    pos->right = p->right;
+    pos->left = p->left;
+    pos = p;
+  }else{
+    while(r->left != NULL){
+      r = r->left;
+    }
+    pos->data = r->data;
+    pos->right = r->right;
+    pos->left = r->left;
+    pos = r;
+  }
+  //把双亲节点的指向设为NULL
+  if(pos->parent->left == pos) pos->parent->left = NULL;
+  else pos->parent->right = NULL;
+  delete pos;
+  pos = NULL;
+  return true;
+}
+
+bool BST::deleteNode(int data){
+  node *p = getNodeByData(root, data);
+  return deleteNodeByPos(p);
+}
+
+//遍历以及ASL相关
+void BST::lnrdfs(node *p, nodeArr *arr){
+  if(p == NULL) return;
+  lnrdfs(p->left, arr);
+  arr->arr[arr->pos++] = p;
+  lnrdfs(p->right, arr);
+}
+
+int *BST::LNRTree(){
+  nodeArr *orderArr= new nodeArr;
+  orderArr->maxsize = nodeNums;
+  orderArr->arr = new node*[nodeNums];
+  lnrdfs(root, orderArr);
+  int *outarr = new int[nodeNums];
+  for(int i = 0; i < nodeNums; i++){
+    outarr[i] = orderArr->arr[i]->data;
+  }
+  return outarr;
+}
+
+int BST::getSL(node *p, int k){
+  int d = k;  //记录当前查找长度
+  if(p->left != NULL){
+    d += getSL(p->left, k + 1);  //把所有左子树的查找长度加进d
+  }
+  if(p->right != NULL){
+    d += getSL(p->right, k + 1);
+  }
+  return d;
+}
+
+float BST::getASL(){
+  return getSL(root) * 1.0 / nodeNums;
+}
+
+/*
+ * AVL相关函数
+*/
+
+void AVL::generateAVL(int list[], int n){
+  for(int i = 1; i < n; i++){
+    insertNode(list[i]);
+    //todo:
+    //检查树是否平衡
+    //旋转
+  }
+}
--- 7BST&AVL/main.cpp
+++ 7BST&AVL/main.cpp
@@ -1,17 +1,17 @@
-#include "link_bst_avl.h"
+#include "linked_bst_avl.h"
 
 int main(){
   //测试链表结构二叉排序树的代码
-  int a[5] = {5,6,3,4,8};
-  BST bst = BST(5);
-  bst.generateBST(a, 5);
-  int *b = bst.LNRTree(5);
+  int a[6] = {5,6,3,4,8,9};
+  BST bst = BST(a[0]);
+  bst.generateBST(a, 6);
+  int *b = bst.LNRTree();
   for(int i = 0; i < bst.nodeNums; i++){
     cout << b[i] << " ";
   }
   cout << endl;
-  if(bst.deleteNodeByPos(bst.getNodeByData(bst.root, 3))){
-    int *c = bst.LNRTree(4);
+  if(bst.deleteNode(4)){
+    int *c = bst.LNRTree();
     for(int i = 0; i < bst.nodeNums; i++){
       cout << c[i] << " ";
     }
